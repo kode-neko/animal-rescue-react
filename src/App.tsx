a774cf05145497f744258a42be10318a119f9429
Kodeneko <ladysun.freedom@gmail.com>
2022-11-18 01:29:32 +0100

    [main] setup project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { Container } from '@mui/material';
 import * as React from 'react';
-import './style.css';
 
 export default function App() {
   return (
-    <Container>
+    <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
-    </Container>
+    </div>
   );
 }
