1b1589668ca0f2c6bc63749f4a698404d23eb4e6
Kodeneko <ladysun.freedom@gmail.com>
2022-11-19 19:59:41 +0100

    [Dev] Included theme & redux providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import { Container } from '@mui/material';
-import * as React from 'react';
+import React, { useMemo } from 'react';
+import { createTheme, CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@emotion/react';
+import { useSelector, Provider } from 'react-redux';
+import { Theme } from './common/model';
+import { RootState, store } from './common/store/store';
 
 export default function App() {
+  const theme = useSelector((state:RootState) => state.user.theme);
+  const themeMUI = useMemo(
+    () => (createTheme({
+      palette: {
+        mode: theme === Theme.DARK ? Theme.DARK : Theme.LIGHT,
+      },
+    })),
+    [theme],
+  );
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <Provider store={store}>
+      <ThemeProvider theme={themeMUI}>
+        <CssBaseline />
+        <div>
+          <h1>Hello StackBlitz!</h1>
+          <p>Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. </p>
+        </div>
+      </ThemeProvider>
+    </Provider>
   );
 }
