3666c5664717f6b68201da1dd412be292e0f4c10
Kodeneko <ladysun.freedom@gmail.com>
2022-11-20 14:08:03 +0100

    [dev] New menu RSS MainBar

--- src/components/MainBar/MainBar.tsx
+++ src/components/MainBar/MainBar.tsx
@@ -5,11 +5,13 @@ import {
   Typography,
   Toolbar,
   IconButton,
-  Button,
+  Menu,
+  MenuItem,
 } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { useTranslation } from 'react-i18next';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
+import ShareIcon from '@mui/icons-material/Share';
 import { MainDrawer } from './MainDrawer';
 import { title } from '../../common/constants';
 import { SwitchTheme } from '../SwitchTheme';
@@ -18,6 +20,7 @@ import { SwitchLang } from '../SwitchLang';
 const MainBar = () => {
   const { t, i18n } = useTranslation();
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isOpenRRSS, setIsOpenRRSS] = useState<boolean>(false);
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const [lang, setLang] = useState<string>('es');
   const handleDrawerToggle = () => setIsOpen(!isOpen);
@@ -25,6 +28,29 @@ const MainBar = () => {
   const handleMenu = (event: React.MouseEvent<HTMLElement>) => {};
   // const handleClose = () => setAnchorEl(null);
   const handleClose = () => {};
+
+  const [anchorElRRSS, setAnchorElRRSS] = React.useState<null | HTMLElement>(null);
+  const openRRSS = Boolean(anchorElRRSS);
+  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElRRSS(event.currentTarget);
+  };
+  const handleMenuCloseRRSS = () => {
+    setAnchorElRRSS(null);
+  };
+
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorElRRSS}
+      id="menuSocial"
+      keepMounted
+      open={openRRSS}
+      onClose={handleMenuCloseRRSS}
+    >
+      <MenuItem onClick={handleMenuCloseRRSS}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuCloseRRSS}>My account</MenuItem>
+    </Menu>
+  );
+
   return (
     <Box sx={{ display: 'flex' }}>
       <AppBar component="nav">
@@ -45,15 +71,28 @@ const MainBar = () => {
           >
             {title}
           </Typography>
-          <Box sx={{ display: { xs: 'none', sm: 'flex', gap: '10px' } }}>
-            <Button
-              startIcon={<AddCircleIcon />}
+          <Box sx={{ display: { xs: 'none', sm: 'flex', gap: '6px' } }}>
+            <IconButton
               size="large"
+              edge="end"
               aria-label="create new animal"
+              aria-controls="add"
+              aria-haspopup="true"
               sx={{ color: 'white' }}
             >
-              {'Añadir'}
-            </Button>
+              <AddCircleIcon />
+            </IconButton>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="watch social media"
+              aria-controls="social"
+              sx={{ color: 'white', marginRight: '4px' }}
+              onClick={handleProfileMenuOpen}
+            >
+              <ShareIcon />
+            </IconButton>
+            {renderMenu}
             <SwitchTheme />
             <SwitchLang />
           </Box>
